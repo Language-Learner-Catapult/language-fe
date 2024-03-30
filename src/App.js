0604e9d40dcf4d8198d46dc8e61e71929210cd78
pkurto16 <ptkurto3@gmail.com>
2024-03-30 11:30:32 -0400

    Start of firebase auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cookies from "js-cookie";
+import Main from "./components/main/Main";
+import SignIn from "./components/sign-in/SignIn";
 
 function App() {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [authenticated, setAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const userId = Cookies.get("user_id");
+        console.log("user_id retrieved from cookie with value: " + userId);
+        if (!userId) {
+            setDialogOpen(true); // If no user_id cookie, open sign-in dialog
+        } else {
+            setAuthenticated(true);
+        }
+    }, [dialogOpen]);
+    const handleClose = (event, reason) => {
+        if (reason && reason === "backdropClick") return;
+        setDialogOpen(false);
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={!authenticated ? "blur-background" : ""}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    <SignIn open={dialogOpen} onClose={handleClose} />
+          </div>
   );
 }
 
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import "src/components/main/Main.css";
+
+function Main() {
+
+    return (
+        <></>
+    );
+}
+
+export default Main;
