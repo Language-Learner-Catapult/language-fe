a1cc869b17a711ea763b5446d7a328d1f501fb69
pkurto16 <ptkurto3@gmail.com>
2024-03-30 18:15:52 -0400

    Bar Cleaned

--- src/components/main/language-selector/LanguageSelector.jsx
+++ src/components/main/language-selector/LanguageSelector.jsx
@@ -4,6 +4,7 @@ import Select from '@mui/material/Select';
 import { LanguageContext } from '../language-context/LanguageContext';
 import languagesData from '../data/languages.json';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 const LanguageSelector = () => {
     const { currentLanguage, setCurrentLanguage } = useContext(LanguageContext);
 
@@ -17,8 +18,13 @@ const LanguageSelector = () => {
             onChange={handleChange}
             displayEmpty
             inputProps={{ 'aria-label': 'Without label' }}
-            IconComponent={ArrowDropDownIcon} // Adding dropdown icon
-            sx={{ color: 'white', '& .MuiSvgIcon-root': { color: 'white' } }}
+            IconComponent={ArrowDropDownIcon}
+            sx={{
+                color: 'white',
+                '& .MuiSvgIcon-root': { color: 'white' },
+                background: 'none', // Ensure no background
+                border: 'none', // Ensure no border
+            }}
         >
             {Object.keys(languagesData).map((language) => (
                 <MenuItem key={language} value={language}>{language}</MenuItem>
