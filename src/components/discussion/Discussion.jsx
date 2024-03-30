ed2c6e79ffebc2d9eb136404563bf1e6eeb503a4
dch777 <d.chanana777@gmail.com>
2024-03-30 19:33:25 -0400

    Added audio functionality

--- src/components/discussion/Discussion.jsx
+++ src/components/discussion/Discussion.jsx
@@ -1,77 +1,108 @@
-import React, { useState, useRef } from 'react';
-import './discussion.css';
-import app from '../../firebaseconfig';
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import React, { useEffect, useRef, useState } from "react";
+import "./discussion.css";
+import app from "../../firebaseconfig";
+import UIFx from "uifx";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 import Cookies from "js-cookie";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
-const Discussion = () => {
-    const db = getFirestore(app);
-    const [talking, setTalking] = useState(false);
-    const mediaRecorderRef = useRef(null);
+const Discussion = (props) => {
+	const db = getFirestore(app);
+	const mediaRecorderRef = useRef(null);
+	const [base64Audio, setAudio] = useState("");
 
-    const startRecording = async () => {
-        setTalking(true);
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            let mimeType = 'audio/wav';
-            if (!MediaRecorder.isTypeSupported(mimeType)) {
-                mimeType = 'audio/webm'; // Fallback to webm if wav is not supported
-            }
-            mediaRecorderRef.current = new MediaRecorder(stream, { mimeType: mimeType });
+	useEffect(() => {
+		if (!Cookies.get("thread_id")) {
+			axios.get("http://localhost:5000/create_thread").then((response) => {
+				Cookies.set("thread_id", response.data, { expires: 1 });
+			});
+		}
+	}, []);
 
-            mediaRecorderRef.current.addEventListener('dataavailable', handleDataAvailable);
-            mediaRecorderRef.current.start();
-        } catch (err) {
-            console.error('Error accessing microphone:', err);
-            setTalking(false); // Stop talking if there is an error
-        }
-    };
+	const startRecording = async () => {
+		props.setTalking(true);
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			let mimeType = "audio/wav";
+			if (!MediaRecorder.isTypeSupported(mimeType)) {
+				mimeType = "audio/webm"; // Fallback to webm if wav is not supported
+			}
+			mediaRecorderRef.current = new MediaRecorder(stream, {
+				mimeType: mimeType,
+			});
 
-    const stopRecording = () => {
-        if (mediaRecorderRef.current) {
-            mediaRecorderRef.current.stop();
-            setTalking(false);
-        }
-    };
+			mediaRecorderRef.current.addEventListener(
+				"dataavailable",
+				handleDataAvailable
+			);
+			mediaRecorderRef.current.start();
+		} catch (err) {
+			console.error("Error accessing microphone:", err);
+			props.setTalking(false); // Stop talking if there is an error
+		}
+	};
 
-    const handleDataAvailable = async (event) => {
-        if (event.data.size > 0) {
-            const audioBlob = new Blob([event.data], { type: 'audio/webm' });
+	const stopRecording = () => {
+		if (mediaRecorderRef.current) {
+			mediaRecorderRef.current.stop();
+			props.setTalking(false);
+		}
+	};
 
-            await uploadAudioToFirebase(audioBlob);
-        }
-    };
+	const handleDataAvailable = async (event) => {
+		if (event.data.size > 0) {
+			const audioBlob = new Blob([event.data], { type: "audio/webm" });
 
-    const uploadAudioToFirebase = async (audioBlob) => {
-        const reader = new FileReader();
+			await uploadAudioToFirebase(audioBlob);
+		}
+	};
 
-        reader.readAsDataURL(audioBlob); // Convert audioBlob to Base64 Data URL
+	const uploadAudioToFirebase = async (audioBlob) => {
+		const reader = new FileReader();
 
-        reader.onloadend = async () => {
-            const base64data = {audio: reader.result};
-            let uuid = uuidv4();
-            await setDoc(
-                doc(
-                    db,
-                    'audioRecords',
-                    Cookies.get("user_id"),
-                    Cookies.get("user_id"),
-                    uuid,
-                ),
-                base64data,
-            );
-        };
-    };
+		reader.readAsDataURL(audioBlob); // Convert audioBlob to Base64 Data URL
 
-    return (<>
-        <div className={`character ${talking ? 'talk' : ''}`}>
-            <div className="mouth"></div>
-        </div>
-    <button onClick={startRecording}>Start Recording</button>
-    <button onClick={stopRecording}>Stop Recording</button>
-        </>
-);
+		reader.onloadend = async () => {
+			const base64data = { audio: reader.result };
+
+			axios
+				.post(
+					"http://localhost:5000/messages/" +
+						Cookies.get("thread_id") +
+						"/send",
+					base64data
+				)
+				.then(async (response) => {
+					const sound = new UIFx(
+						"data:audio/webm;base64," + response.data.audio,
+						{
+							volume: 0.8,
+						}
+					);
+					sound.play();
+				});
+
+			let uuid = uuidv4();
+			await setDoc(
+				doc(
+					db,
+					"audioRecords",
+					Cookies.get("user_id"),
+					Cookies.get("user_id"),
+					uuid
+				),
+				base64data
+			);
+		};
+	};
+
+	return (
+		<>
+			<button onClick={startRecording}>Start Recording</button>
+			<button onClick={stopRecording}>Stop Recording</button>
+		</>
+	);
 };
 
 export default Discussion;
--- src/components/main/profile-card/ProfileCard.jsx
+++ src/components/main/profile-card/ProfileCard.jsx
@@ -1,81 +1,80 @@
-import React, { useContext } from 'react';
-import { Typography, Box } from '@mui/material';
-import { LanguageContext } from '../language-context/LanguageContext';
-import languagesData from '../data/languages.json';
-import Discussion from '../../discussion/Discussion.jsx';
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useContext, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { LanguageContext } from "../language-context/LanguageContext";
+import languagesData from "../data/languages.json";
+import Discussion from "../../discussion/Discussion.jsx";
+import { AnimatePresence, motion } from "framer-motion";
 
 const ProfileCard = () => {
-    const { currentLanguage } = useContext(LanguageContext);
-    const profileData = {
-        name: languagesData[currentLanguage].agentName,
-        // avatarUrl will not be used for the image anymore
-    };
+	const { currentLanguage } = useContext(LanguageContext);
+	const [talking, setTalking] = useState(false);
+	const profileData = {
+		name: languagesData[currentLanguage].agentName,
+		// avatarUrl will not be used for the image anymore
+	};
 
-    // Function to generate random animation for each bar
-    const generateAnimation = () => ({
-        hidden: { scaleY: 0.2 },
-        show: {
-            scaleY: [0.2, Math.random() + 0.5, Math.random()],
-            transition: {
-                duration: Math.random() * 0.5 + 0.5, // Random duration between 0.5 to 1 second
-                ease: 'easeInOut',
-                repeat: Infinity,
-                repeatType: 'mirror',
-            }
-        }
-    });
+	// Function to generate random animation for each bar
+	const generateAnimation = () => ({
+		hidden: { scaleY: 0.2 },
+		show: {
+			scaleY: [0.2, Math.random() + 0.5, Math.random()],
+			transition: {
+				duration: Math.random() * 0.5 + 0.5, // Random duration between 0.5 to 1 second
+				ease: "easeInOut",
+				repeat: Infinity,
+				repeatType: "mirror",
+			},
+		},
+	});
 
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                textAlign: 'center',
-                color: 'white',
-            }}
-        >
-            <Typography variant="h5" component="h2" gutterBottom>
-                {profileData.name}
-            </Typography>
-            {/* Animated Bars with Separate Random Animations */}
-            <Box
-                sx={{
-                    display: 'flex',
-                    height: 180,
-                    margin: '8px 0',
-                }}
-            >
-                <AnimatePresence>
-                    {Array.from({ length: 4 }).map((_, index) => (
-                        <motion.div
-                            key={`bar-${index}`}
-                            style={{
-                                background: 'white',
-                                width: '20px',
-                                height: '100%',
-                                margin: '0 2px',
-                                borderRadius: '10px',
-                                originY: 1, // Animation origin at the bottom
-                            }}
-                            variants={generateAnimation()} // Call generateAnimation to get unique animations
-                            initial="hidden"
-                            animate="show"
-                        />
-                    ))}
-                </AnimatePresence>
-            </Box>
-            <Discussion/>
-            <Typography variant="subtitle1">
-                Learning {currentLanguage}
-            </Typography>
-        </Box>
-    );
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				position: "absolute",
+				top: "50%",
+				left: "50%",
+				transform: "translate(-50%, -50%)",
+				textAlign: "center",
+				color: "white",
+			}}
+		>
+			<Typography variant="h5" component="h2" gutterBottom>
+				{profileData.name}
+			</Typography>
+			{/* Animated Bars with Separate Random Animations */}
+			<Box
+				sx={{
+					display: "flex",
+					height: 180,
+					margin: "8px 0",
+				}}
+			>
+				<AnimatePresence>
+					{Array.from({ length: 4 }).map((_, index) => (
+						<motion.div
+							key={`bar-${index}`}
+							style={{
+								background: "white",
+								width: "20px",
+								height: "100%",
+								margin: "0 2px",
+								borderRadius: "10px",
+								originY: 1, // Animation origin at the bottom
+							}}
+							variants={generateAnimation()} // Call generateAnimation to get unique animations
+							initial="hidden"
+							animate="show"
+						/>
+					))}
+				</AnimatePresence>
+			</Box>
+			<Discussion setTalking={setTalking} />
+			<Typography variant="subtitle1">Learning {currentLanguage}</Typography>
+		</Box>
+	);
 };
 
 export default ProfileCard;
